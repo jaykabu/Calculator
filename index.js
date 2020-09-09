d9276f22c233d90c37515e109eed1d88815ae812
jaykabu <59605424+jaykabu@users.noreply.github.com>
2020-09-09 15:47:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+/* simple meg pass
+
+// ReactDom.render(<React.Fragment>
+//     <h1> Hello world. </h1>
+//     <p> React </p>
+//     <h2> REACT </h2> </React.Fragment> , document.getElementById('root'));
+
+*/
+
+/*  challenge
+
+ReactDom.render(<React.Fragment>
+  <h1> Netflix </h1>
+ <p> list of best five serise </p>
+  <ol>
+    <li> dark</li>
+    <li> asur</li>
+    <li> familyman</li>
+    <li> stranger</li>
+    <li> ironman </li>
+  </ol>
+
+</React.Fragment> , document.getElementById('root'));
+
+*/
+
+/*
+//Expression
+const fname = "Jay";
+ReactDom.render(<>
+  <h1> My name is {fname}</h1>
+  <p>JavaScript Expression. my lucky number is {Math.random()}</p>
+</>
+    ,document.getElementById('root'));
+
+ */
